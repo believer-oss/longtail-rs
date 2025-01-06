bb5cf91bee7558e5b02c16ec60d382fd7fad08f4
Chris Maynard <127986818+chris-believer@users.noreply.github.com>
2025-01-06 11:50:09 -0500

    tests: NativeBuffer tests

--- src/lib.rs
+++ src/lib.rs
@@ -13,29 +13,22 @@
 //! code be more idiomatic Rust.
 
 use longtail_sys::*;
-
 use std::ffi::c_void;
-
 use strum::{EnumString, FromRepr};
 
 pub mod longtaillib;
-
 pub use longtaillib::*;
 
 pub mod longtailstorelib;
-
 pub use longtailstorelib::*;
 
 pub mod remotestore;
-
 pub use remotestore::*;
 
 pub mod path_filter;
-
 pub use path_filter::RegexPathFilter;
 
 pub mod async_apis;
-
 pub use async_apis::*;
 
 pub mod commands;
@@ -71,5 +64,48 @@ impl NativeBuffer {
         //! Safety: The buffer is a valid pointer
         assert!(!self.buffer.is_null());
         unsafe { std::slice::from_raw_parts(self.buffer as *const u8, self.size) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::NativeBuffer;
+    use std::mem;
+    use std::os::raw::c_void;
+
+    #[test]
+    fn test_new() {
+        let buffer = NativeBuffer::new();
+        assert!(buffer.buffer.is_null());
+        assert_eq!(buffer.size, 0);
+    }
+
+    #[test]
+    fn test_as_slice() {
+        let mut buffer = NativeBuffer::new();
+        let data = vec![1u8, 2, 3];
+        let len = data.len();
+        buffer.buffer = Box::into_raw(data.into_boxed_slice()) as *mut c_void;
+        buffer.size = len;
+        let slice = buffer.as_slice();
+        assert_eq!(slice, &[1, 2, 3]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_as_slice_panic() {
+        let buffer = NativeBuffer::new();
+        let _ = buffer.as_slice();
+    }
+
+    #[test]
+    fn test_drop() {
+        let mut buffer = NativeBuffer::new();
+        let data = vec![1u8, 2, 3];
+        buffer.size = data.len();
+        buffer.buffer = Box::into_raw(data.into_boxed_slice()) as *mut c_void;
+        mem::drop(buffer);
+        // If the buffer was correctly dropped, we should be able to create a new one without any issues.
+        let _new_buffer = NativeBuffer::new();
     }
 }
